b99f5eb4fe58a018069887a86b17a4d53b10587d
Łukasz Wilk <107866108+LukaszWilk1@users.noreply.github.com>
2023-08-03 09:53:06 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ class GameArea{
       this.canvas.height=height;
       document.body.appendChild(this.canvas);
       this.cx = this.canvas.getContext("2d");
-      this.interval = setInterval(updateGame, 20);
   }
   clear(){
     this.cx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -39,8 +38,6 @@ class Player{
     this.width=width;
     this.height = height;
     this.color=color;
-    this.speedX=0;
-    this.speedY=0;
   }
   update(){
     this.body = canva.cx;
@@ -50,54 +47,133 @@ class Player{
   clear(){
     this.body.clearRect(this.x, this.y, this.width, this.height);
   }
-  newPos(){
-    this.x+=this.speedX;
-    this.y+=this.speedY;
+}
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
   }
-  colision(otherObj){
-    let playerLeft = this.x;
-    let playerTop = this.y;
-    let playerRight = this.x+this.width;
-    let playerBottom = this.y+this.heigt;
-    let otherLeft = otherObj.x;
-    let otherTop = otherObj.y;
-    let otherRight = otherObj.x+otherObj.width;
-    let otherBottom = otherObj.y+otherObj.height;
-    let crash = true;
-    if(playerBottom<otherTop || playerRight<otherLeft || playerTop>otherBottom || playerLeft>otherRight){
-      crash = false;
-    }
-    return crash;
+}
+
+function collisions({oPlayer, otherObj}){
+  let playerLeft = player.x;
+  let playerTop = player.y;
+  let playerRight = player.x+player.width;
+  let playerBottom = player.y+player.height;
+  let otherLeft = otherObj.x;
+  let otherTop = otherObj.y;
+  let otherRight = otherObj.x+otherObj.width;
+  let otherBottom = otherObj.y+otherObj.height;
+  if(playerRight>otherLeft && playerLeft < otherRight && playerTop < otherBottom && playerBottom > otherTop){
+    return true;
   }
 }
 
 let player = new Player(100,100,30,30,"black");
 player.update();
 
+const boundaries = [new Player(200, 200, 50, 50, "yellow"), new Player(350, 200, 50, 50, "black")];
+
 let obstacle = new Player(200, 200, 50, 50, "yellow");
+obstacle.update();
 
 function updateGame(){
-  if(player.colision(obstacle)){
-    player.speedX=0;
-    player.speedY=0;
-  }
-  canva.clear();
+  let movable=true;
+  window.requestAnimationFrame(updateGame);
   canva.loadMap();
-  obstacle.update();
-  player.newPos();
+  boundaries.forEach(boundary => {
+    boundary.update();
+  })
   player.update();
+
+    if(keys.w.pressed && lastKey==='w'){
+      for(let i=0;i<boundaries.length;i++){
+        let obstacle = boundaries[i];
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x, y: obstacle.y+5}})){
+          movable=false;
+        }
+      }
+      if(movable) player.y-=5;
+    }
+    else if(keys.s.pressed && lastKey==='s'){
+      for(let i=0;i<boundaries.length;i++){
+        let obstacle = boundaries[i];
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x, y: obstacle.y-5}})){
+          movable=false;
+        }
+      }
+      if(movable) player.y+=5;
+    }
+    else if(keys.a.pressed && lastKey==='a'){
+      for(let i=0;i<boundaries.length;i++){
+        let obstacle = boundaries[i];
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x+5, y: obstacle.y}})){
+          movable=false;
+        }
+      }
+        if(movable) player.x-=5;
+    }
+    else if(keys.d.pressed && lastKey==='d'){
+      for(let i=0;i<boundaries.length;i++){
+        let obstacle = boundaries[i];
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x-5, y: obstacle.y}})){
+          movable=false;
+        }
+      }
+       if(movable) player.x+=5;
+    }
 }
 
-document.addEventListener("keydown", e => {
-  if(e.key=="d") player.speedX=20;
-  if(e.key=="a") player.speedX=-20;
-  if(e.key=="s") player.speedY=20;
-  if(e.key=="w") player.speedY=-20;
+updateGame();
+
+let lastKey;
+window.addEventListener("keydown", e => {
+  switch(e.key){
+    case "w":
+      keys.w.pressed=true;
+      lastKey='w';
+      break;
+    case "a":
+      keys.a.pressed=true;
+      lastKey='a';
+      break;
+    case "s":
+      keys.s.pressed=true;
+      lastKey='s';
+      break;
+    case "d":
+      keys.d.pressed=true;
+      lastKey='d';
+      break;
+  }
 })
 
-document.addEventListener("keyup", e => {
-  if(e.key=="d") player.speedX=0;
-  if(e.key=="a") player.speedX=0;
-  if(e.key=="s") player.speedY=0;
-  if(e.key=="w") player.speedY=0;
+window.addEventListener("keyup", e => {
+  switch(e.key){
+    case "w":
+      keys.w.pressed=false;
+      player.speedY=0;
+      break;
+    case "a":
+      keys.a.pressed=false;
+      player.speedX=0;
+      break;
+    case "s":
+      keys.s.pressed=false;
+      player.speedY=0;
+      break;
+    case "d":
+      keys.d.pressed=false;
+      player.speedX=0;
+      break;
+  }
 })
