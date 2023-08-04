13cbbdf6ae74da97f36beca5dac49330af29ae9c
Łukasz Wilk <107866108+LukaszWilk1@users.noreply.github.com>
2023-08-04 10:49:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ class GameArea{
       document.body.appendChild(this.canvas);
       this.cx = this.canvas.getContext("2d");
   }
+  getHeight(){
+    return this.canvas.height;
+  }
   clear(){
     this.cx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
 
+GameArea.prototype.getWidth = function(){
+  return this.canvas.width;
+}
+
 GameArea.prototype.loadMap = function(){
+  /*
   let count = 0;
   for(let i=0; i<=480;i+=20){
     for(let j=0; j<=720;j+=20){
@@ -26,12 +34,49 @@ GameArea.prototype.loadMap = function(){
       }
     }
   }
+  */
 }
+
+const myImage = new Image(3840, 2160);
+myImage.src="pobrane.jpg";
+
+class Background{
+constructor(image){
+  this.image=image;
+    this.x=-100;
+    this.y=-100;
+  }
+  updateBackground(){
+    canva.cx.drawImage(this.image, this.x, this.y, 3840, 2160);
+  }
+}
+
+const background = new Background(myImage);
 
 let canva = new GameArea(720,480);
 canva.loadMap();
 
 class Player{
+  constructor(x, y, width, height, color){
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height = height;
+    this.color=color;
+  }
+  update(){
+    this.x=(canva.getWidth()/2)-(this.width/2);
+    this.y=(canva.getHeight()/2)-(this.height/2);
+    this.body = canva.cx;
+    this.body.fillStyle = this.color;
+    this.body.fillRect((canva.getWidth()/2)-(this.width/2),(canva.getHeight()/2)-(this.height/2), this.width, this.height);
+  }
+  clear(){
+    this.body.clearRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+class Obstacles{
   constructor(x, y, width, height, color){
     this.x=x;
     this.y=y;
@@ -78,18 +123,18 @@ function collisions({oPlayer, otherObj}){
   }
 }
 
-let player = new Player(100,100,30,30,"black");
+let player = new Player(0,0,30,30,"black");
 player.update();
 
-const boundaries = [new Player(200, 200, 50, 50, "yellow"), new Player(350, 200, 50, 50, "black")];
+const boundaries = [new Obstacles(200, 200, 50, 50, "rgba(255, 255, 255, 0.1)"), new Obstacles(400, 200, 50, 50, "rgba(255, 255, 255, 0.1)")];
+const movingObjects = [background, ...boundaries];
 
-let obstacle = new Player(200, 200, 50, 50, "yellow");
-obstacle.update();
 
 function updateGame(){
   let movable=true;
   window.requestAnimationFrame(updateGame);
-  canva.loadMap();
+  canva.clear();
+  background.updateBackground();
   boundaries.forEach(boundary => {
     boundary.update();
   })
@@ -98,38 +143,54 @@ function updateGame(){
     if(keys.w.pressed && lastKey==='w'){
       for(let i=0;i<boundaries.length;i++){
         let obstacle = boundaries[i];
-        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x, y: obstacle.y+5}})){
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x, y: obstacle.y+2.5}})){
           movable=false;
         }
       }
-      if(movable) player.y-=5;
+      if(movable){
+        for(let i=0;i<movingObjects.length;i++){
+          movingObjects[i].y+=3;
+        }
+      }
     }
     else if(keys.s.pressed && lastKey==='s'){
       for(let i=0;i<boundaries.length;i++){
         let obstacle = boundaries[i];
-        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x, y: obstacle.y-5}})){
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x, y: obstacle.y-2.5}})){
           movable=false;
         }
       }
-      if(movable) player.y+=5;
+      if(movable){
+        for(let i=0;i<movingObjects.length;i++){
+          movingObjects[i].y-=3;
+        }
+      }
     }
     else if(keys.a.pressed && lastKey==='a'){
       for(let i=0;i<boundaries.length;i++){
         let obstacle = boundaries[i];
-        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x+5, y: obstacle.y}})){
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x+2.5, y: obstacle.y}})){
           movable=false;
         }
       }
-        if(movable) player.x-=5;
+        if(movable){
+          for(let i=0;i<movingObjects.length;i++){
+            movingObjects[i].x+=3;
+          }
+        }
     }
     else if(keys.d.pressed && lastKey==='d'){
       for(let i=0;i<boundaries.length;i++){
         let obstacle = boundaries[i];
-        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x-5, y: obstacle.y}})){
+        if(collisions({oPlayer: player, otherObj:{...obstacle, x: obstacle.x-2.5, y: obstacle.y}})){
           movable=false;
         }
       }
-       if(movable) player.x+=5;
+       if(movable){
+         for(let i=0;i<movingObjects.length;i++){
+           movingObjects[i].x-=3;
+         }
+       }
     }
 }
 
